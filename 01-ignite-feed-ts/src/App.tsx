fc1637af119b657d1d0b8c87840470d97b66f7bf
Matheus Falavigna <matheusfalavigna@gmail.com>
2025-01-22 15:49:18 -0300

    feat: add App component and styling

---
+++ 01-ignite-feed-ts/src/App.tsx
@@ -0,0 +1,64 @@
+import styles from "./App.module.css";
+import Header from "./components/Header/Header";
+import Post, { PostType } from "./components/Post/Post";
+import Sidebar from "./components/Sidebar/Sidebar";
+import "./global.css";
+
+const posts: PostType[] = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: "https://github.com/diego3g.png",
+      name: "Diego",
+      role: "CTO",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "diego.dev/rocketseat" },
+    ],
+    publishedAt: new Date("2025-01-21 13:42:00"),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: "https://github.com/maykbrito.png",
+      name: "Maik Brito",
+      role: "Instrutor na Rocketseat",
+    },
+    content: [
+      { type: "paragraph", content: "Fala galeraa 👋" },
+      {
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      { type: "link", content: "jane.dev/rocketseat" },
+    ],
+    publishedAt: new Date("2025-01-21 20:42:00"),
+  },
+];
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+
+        <main>
+          {posts.map((post) => (
+            <Post key={post.id} post={post} />
+          ))}
+        </main>
+      </div>
+    </>
+  );
+}
+
+export default App;
