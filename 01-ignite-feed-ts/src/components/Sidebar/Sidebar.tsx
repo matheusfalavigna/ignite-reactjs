526191302eb0d4d3978f08a7da0ba8737ec3065e
Matheus Falavigna <matheusfalavigna@gmail.com>
2025-01-22 15:50:00 -0300

    feat: add Sidebar component and styling

---
+++ 01-ignite-feed-ts/src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,27 @@
+import { PencilLine } from "phosphor-react";
+import Avatar from "../Avatar/Avatar";
+import styles from "./Sidebar.module.css";
+
+export default function Sidebar() {
+  return (
+    <aside className={styles.sidebar}>
+      <img
+        className={styles.cover}
+        src="https://plus.unsplash.com/premium_photo-1678565999332-1cde462f7b24?q=50&w=500"
+      />
+
+      <div className={styles.profile}>
+        <Avatar src="https://avatars.githubusercontent.com/u/60001410?v=4" />
+        <strong>Matheus Falavigna</strong>
+        <span>Desenvolvedor Full Stack</span>
+      </div>
+
+      <footer>
+        <a>
+          <PencilLine style={{ marginRight: "0.5rem" }} size={20} />
+          Editar perfil
+        </a>
+      </footer>
+    </aside>
+  );
+}
