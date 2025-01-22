bd53d777a209ba364a39fc445ba622a7a145c313
Matheus Falavigna <matheusfalavigna@gmail.com>
2025-01-22 15:49:35 -0300

    feat: add Comment component and styling

---
+++ 01-ignite-feed-ts/src/components/Comment/Comment.tsx
@@ -0,0 +1,59 @@
+import { ThumbsUp, Trash } from "phosphor-react";
+import { useState } from "react";
+import Avatar from "../Avatar/Avatar";
+import styles from "./Comment.module.css";
+
+interface CommentProps {
+  content: string;
+  onDeleteComment: (comment: string) => void;
+}
+
+export default function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
+
+  function handleDeleteComment() {
+    onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    });
+  }
+
+  return (
+    <div className={styles.comment}>
+      <Avatar
+        src="https://avatars.githubusercontent.com/u/60001410?v=4"
+        alt=""
+        hasBorder={false}
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>Matheus Falavigna</strong>
+              <time title="11 de Maio às 08:13h" dateTime="2024-05-11 08:13:30">
+                Cerca de 1h atrás
+              </time>
+            </div>
+
+            <button title="Deletar comentário" onClick={handleDeleteComment}>
+              <Trash size={24} />
+            </button>
+          </header>
+
+          <p>{content}</p>
+        </div>
+
+        <footer>
+          <button onClick={handleLikeComment}>
+            <ThumbsUp />
+            Aplaudir <span>{likeCount}</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  );
+}
