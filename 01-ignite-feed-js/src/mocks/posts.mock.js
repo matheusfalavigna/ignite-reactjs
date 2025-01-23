288837196b86f0b62ec706fa601575d3177e049b
Matheus Falavigna <matheusfalavigna@gmail.com>
2025-01-23 17:48:16 -0300

    feat: add mocked data to posts

---
+++ 01-ignite-feed-js/src/mocks/posts.mock.js
@@ -0,0 +1,58 @@
+import { faker } from "@faker-js/faker";
+import { commentMock } from "./comment.mock";
+
+function getPublishedDate(name) {
+  if (name.length % 2 === 0) {
+    return faker.date.past();
+  }
+
+  return faker.date.recent();
+}
+
+export const postMock = ({ isFirst = false }) => {
+  const author = isFirst
+    ? {
+        name: "Diego Fernandes",
+        avatar: "https://github.com/diego3g.png",
+        role: "CTO",
+      }
+    : {
+        name: faker.person.fullName(),
+        avatar: faker.image.avatar(),
+        role: faker.person.jobTitle(),
+      };
+
+  let fullName = author.name;
+  let firstName = fullName.split(" ")[0].toLowerCase();
+
+  return {
+    id: faker.string.uuid(),
+    author: {
+      avatarUrl: author.avatar,
+      name: author.name,
+      role: author.role,
+    },
+    publishAt: getPublishedDate(author.name),
+    content: [
+      {
+        id: faker.string.uuid(),
+        type: "paragraph",
+        content: "Fala galera 👋",
+      },
+      {
+        id: faker.string.uuid(),
+        type: "paragraph",
+        content:
+          "Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀",
+      },
+      {
+        id: faker.string.uuid(),
+        type: "link",
+        content: `👉 ${firstName}.design/doctorcare`,
+      },
+    ],
+    comments: Array.from({ length: faker.number.int({ min: 1, max: 2 }) }, () =>
+      commentMock()
+    ),
+  };
+};
