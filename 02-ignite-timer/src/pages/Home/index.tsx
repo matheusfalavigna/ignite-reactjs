34b135df8c27d52da4b3c2901efcb3402b3dd66c
Matheus Falavigna <matheusfalavigna@gmail.com>
2025-03-12 17:57:32 -0300

    feat: add content and styles

--- 02-ignite-timer/src/pages/Home/index.tsx
+++ 02-ignite-timer/src/pages/Home/index.tsx
@@ -1,3 +1,59 @@
+import { Play } from "phosphor-react";
+import {
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
+} from "./styles";
+
 export function Home() {
-  return <h1>Home</h1>;
+  return (
+    <HomeContainer>
+      <form action="">
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            list="tasks"
+            type="text"
+            placeholder="Dê um nome para a sua tarefa"
+          />
+
+          <datalist id="tasks">
+            <option value="Estudar" />
+            <option value="Trabalhar" />
+            <option value="Exercícios" />
+            <option value="Ler" />
+          </datalist>
+
+          <label htmlFor="minutesAmount">por</label>
+          <MinutesAmountInput
+            id="minutesAmount"
+            type="number"
+            placeholder="00"
+            step={5}
+            min={5}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play /> Começar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
+  );
 }
